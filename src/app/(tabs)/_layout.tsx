9f318849432e3aee20774e13a0d10b1f94b00009
Agustinus Nathaniel <berserk.nate@gmail.com>
2024-02-28 15:26:01 +0700

    chore: move to src folder

---
+++ src/app/(tabs)/_layout.tsx
@@ -0,0 +1,33 @@
+import { Link, Tabs } from "expo-router";
+import { Button, Text } from "tamagui";
+
+export default function TabLayout() {
+  return (
+    <Tabs
+      screenOptions={{
+        tabBarActiveTintColor: "blue",
+        tabBarInactiveTintColor: "#4e4e4e",
+      }}
+    >
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: "Tab One",
+          tabBarIcon: ({ color }) => <Text>Hello! {color}</Text>,
+          headerRight: () => (
+            <Link href="/modal" asChild>
+              <Button size="$2" chromeless>Press Me!</Button>
+            </Link>
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="two"
+        options={{
+          title: "Tab Two",
+          tabBarIcon: ({ color }) => <Text>Hello! {color}</Text>,
+        }}
+      />
+    </Tabs>
+  );
+}
--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -3,7 +3,7 @@ import { Text, View } from 'tamagui'
 export default function TabOneScreen() {
   return (
     <View flex={1} alignItems="center">
-      <Text fontSize={20}>Tab One</Text>
+      <Text fontSize={20}>Tab ONEE</Text>
     </View>
   )
 }
