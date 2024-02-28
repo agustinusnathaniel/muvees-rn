4c3a474eef5297847143193aaf8254af8819a4cf
Agustinus Nathaniel <berserk.nate@gmail.com>
2024-02-28 18:00:23 +0700

    refactor: install biome

--- src/app/(tabs)/_layout.tsx
+++ src/app/(tabs)/_layout.tsx
@@ -1,18 +1,18 @@
-import { Link, Tabs } from "expo-router";
-import { Button, Text } from "tamagui";
+import { Link, Tabs } from 'expo-router'
+import { Button, Text } from 'tamagui'
 
 export default function TabLayout() {
   return (
     <Tabs
       screenOptions={{
-        tabBarActiveTintColor: "blue",
-        tabBarInactiveTintColor: "#4e4e4e",
+        tabBarActiveTintColor: 'blue',
+        tabBarInactiveTintColor: '#4e4e4e',
       }}
     >
       <Tabs.Screen
         name="index"
         options={{
-          title: "Tab One",
+          title: 'Tab One',
           tabBarIcon: ({ color }) => <Text>Hello! {color}</Text>,
           headerRight: () => (
             <Link href="/modal" asChild>
@@ -26,10 +26,10 @@ export default function TabLayout() {
       <Tabs.Screen
         name="two"
         options={{
-          title: "Tab Two",
+          title: 'Tab Two',
           tabBarIcon: ({ color }) => <Text>Hello! {color}</Text>,
         }}
       />
     </Tabs>
-  );
+  )
 }
